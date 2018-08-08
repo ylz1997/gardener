60ac95f63a94f1fe038b5959e0e85e0ba916e847
敬伟 <jingwei@asiainfo.com>
2018-08-09 00:29:08 +0800

    11

--- src/main/java/com/jw/kids/bean/TKidsVO.java
+++ src/main/java/com/jw/kids/bean/TKidsVO.java
@@ -10,6 +10,9 @@ import com.jw.base.GeneralException;
 public class TKidsVO extends TKids{
     private String strCrtTime;
 
+
+    private String strBirthday;
+
     public String getStrCrtTime() {
         return strCrtTime;
     }
@@ -17,5 +20,14 @@ public class TKidsVO extends TKids{
     public void setStrCrtTime(String strCrtTime) throws GeneralException {
         this.strCrtTime = strCrtTime;
         setCrtTime(DateUtil.convertDateStringToTimestamp(strCrtTime, "yyyy-MM-dd HH:mm:ss"));
+    }
+
+    public String getStrBirthday() {
+        return strBirthday;
+    }
+
+    public void setStrBirthday(String strBirthday) throws GeneralException {
+        this.strBirthday = strBirthday;
+        setBirthday(DateUtil.convertDateStringToTimestamp(strBirthday, "yyyy-MM-dd"));
     }
 }
